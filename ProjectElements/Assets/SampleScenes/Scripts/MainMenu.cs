ba35812268382f3606ca73ea32f1f673a3326503
Bartomeu <bartomeu.puig@enti.cat>
2020-05-18 19:38:16 +0200

    main menu

---
+++ ProjectElements/Assets/SampleScenes/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private void Start()
+    {
+        transform.GetChild(4).gameObject.GetComponent<UnityEngine.UI.Button>().interactable =
+            !PlayerPrefs.HasKey("LEVEL2") || PlayerPrefs.GetInt("LEVEL2") == 0 ? false : true;
+    }
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+    public void showOptions()
+    {
+        for (int i = 1; i < transform.childCount; i++) transform.GetChild(i).gameObject.SetActive(false);
+    }
+}
