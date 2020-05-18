706efc4370e963a238159a87543bbe8b480c6502
Nil Baro <nil.baro@enti.cat>
2020-05-18 19:07:47 +0200

    Shit

---
+++ ProjectElements/Assets/EndController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "End")
+        {
+            if(SceneManager.GetActiveScene().name == "Sewer")
+            {
+                SceneManager.LoadScene("Nexo");
+            }
+            else if (SceneManager.GetActiveScene().name == "Nexo")
+            {
+                SceneManager.LoadScene("End");
+            }
+        }
+    }
+}
